8d0f41246ac3fe06065018c89b548a6c247732ff
arfakl99 <muhammadarifnust99@gmail.com>
2023-01-16 16:12:31 +0500

    created nested pages and dynamic routes

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <div>Thisdjksdkbdjnvb sdnsdjhgv mcbvjhgchusdgc cnsdcvsghchv dchsdgcfhsdgcnzxb cdxhcvgh </div>
+    
+    </>
+  )
+}
+
+
+export default Home
---
+++ pages/product/[productid].js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Productid = () => {
+  return (
+    <div>Productid kdjishd sdjhsgjhv</div>
+  )
+}
+
+export default Productid
